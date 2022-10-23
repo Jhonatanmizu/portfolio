556e990bdef7475756243d1d5a5d89471579a1df
Jhonatanmizu <natanjesuss20@gmail.com>
2022-10-22 23:36:35 -0300

    :sparkles: added banner image

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { useState } from "react";
+import { ThemeProvider } from "styled-components";
 import "./App.css";
+import Main from "./components/Main";
 
 import Navigation from "./components/Navigation";
+import dark from "./styles/theme/dark";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <section id="home">
-        <a href="https://jacinto.design" target="_blank">
-          Jhonatan Jesus
-        </a>
-      </section>
-      <section id="about">
-        <h1>Learn More About Me</h1>
-      </section>
-      <section id="skills">
-        <h1>These Are My Strengths</h1>
-      </section>
-      <section id="projects">
-        <h1>These Are My Results</h1>
-      </section>
-      <section id="contact">
-        <h1>Available Anytime</h1>
-      </section>
-    </div>
+    <ThemeProvider theme={dark}>
+      <div className="App">
+        <Navigation />
+        <Main />
+        <section id="about">
+          <h1>Learn More About Me</h1>
+        </section>
+        <section id="skills">
+          <h1>These Are My Strengths</h1>
+        </section>
+        <section id="projects">
+          <h1>These Are My Results</h1>
+        </section>
+        <section id="contact">
+          <h1>Available Anytime</h1>
+        </section>
+      </div>
+    </ThemeProvider>
   );
 }
 
